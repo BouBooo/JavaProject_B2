e7fd294e7ef11f18d2ade08101b8f55b5704a3c8
BouBooo <flonicolas.pro@outlook.fr>
2019-04-19 11:02:18 +0200

    Mysql configuration & test connection

---
+++ src/ynov/clientserver/db/connector/MySqlConnecteur.java
@@ -0,0 +1,63 @@
+package ynov.clientserver.db.connector;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class MySqlConnecteur {
+	private Connection con;
+	
+	public MySqlConnecteur(String IP, String base, String login, String pass)
+	{
+		try
+		{
+			String url = "jdbc:mysql://"+IP+"/"+base; 	//Database path		  
+			con=DriverManager.getConnection(url, login, pass);
+		}
+		catch(Exception exc)
+		{
+			System.err.println(exc.getMessage());
+		}
+
+
+	}
+	
+	public MySqlConnecteur(String base)
+	{
+		this("localhost",base,"admin","admin");
+	}
+	
+	public ResultSet select(String sql)
+	{
+		// SQL Request
+		try
+		{	
+			Statement instruction=con.createStatement();
+			ResultSet rs = instruction.executeQuery(sql);
+			return rs;
+		}
+		catch(Exception exc)
+		{
+			System.err.println(exc.getMessage());
+			return null;
+		}
+
+	}
+	
+	public int maj(String sql)
+	{
+		try
+		{	
+			Statement instruction=con.createStatement();
+			int rs = instruction.executeUpdate(sql);
+			return rs;
+		}
+		catch(Exception exc)
+		{
+			System.err.println(exc.getMessage());
+			return -1;
+		}
+	}
+}
---
+++ src/ynov/clientserver/db/launchers/TestConnection.java
@@ -0,0 +1,38 @@
+package ynov.clientserver.db.launchers;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.ResultSet;
+
+import ynov.clientserver.db.connector.MySqlConnecteur;
+
+public class TestConnection {
+
+	public static void main(String[] args) {
+		try
+		{
+			MySqlConnecteur mc=new MySqlConnecteur("anniversaires");
+			
+			// SQL Request
+			String sql = "SELECT * FROM anniv";
+			ResultSet rs = mc.select(sql);
+			
+			// View
+			while(rs.next())
+			{
+				String prenom = rs.getString("prenom");
+				String nom = rs.getString("nom");
+				Date annee = rs.getDate("anneeNaissance");
+				int sexe = rs.getInt("sexe");
+				
+				System.out.print(prenom + " - " + nom + " : " + annee);
+			}
+		}
+		catch(Exception exc)
+		{
+			System.err.println(exc.getMessage());
+		}
+
+	}
+
+}
