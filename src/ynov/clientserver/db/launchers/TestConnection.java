d86a70727f996c251c7f3866d0076ddf70923bb2
BouBooo <flonicolas.pro@outlook.fr>
2019-05-08 16:44:14 +0200

    2 sql requests done

--- src/ynov/clientserver/db/launchers/TestConnection.java
+++ src/ynov/clientserver/db/launchers/TestConnection.java
@@ -3,6 +3,7 @@ package ynov.clientserver.db.launchers;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -14,36 +15,50 @@ public class TestConnection {
 	public static JSONArray recupere(JSONObject demande) {
 		JSONArray res = new JSONArray();
 		MySqlConnecteur conn=new MySqlConnecteur("anniversaires");
-		// on récupère de demande la commande
+
 		String commande=demande.getString("commande");
+		
+		// All table datas
 		if(commande.equals("*")) {
 			String sql="SELECT * FROM anniv";
 			try {
 				ResultSet rs=conn.select(sql);
 				while(rs.next()) {
 					JSONObject record=new JSONObject();
-					// récupérer les données -> jsonobject record
+					
+					// SQL Results
 					String prenom = rs.getString("prenom");
 					String nom = rs.getString("nom");
 					int annee = rs.getInt("anneeNaissance");
 					record.put("Prénom", prenom);
 					record.put("Nom", nom);
 					record.put("Année", annee);
-					// ajouter record au jsonarray
 					res.put(record);
 				}
 		
 			} catch(Exception e) {
-				
+				System.out.println(e.getMessage());
 			}
 		}
+		// All the people born <= annee
 		else if(commande.equals("<=")) {
 			int annee=demande.getInt("valeur");
 			String sql="SELECT * FROM anniv WHERE anneeNaissance<="+annee;
 			try {
-				
+				ResultSet rs=conn.select(sql);
+				while(rs.next()) {
+					JSONObject record=new JSONObject();
+	
+					String prenom = rs.getString("prenom");
+					String nom = rs.getString("nom");
+					int anneeNaissance = rs.getInt("anneeNaissance");
+					record.put("Prénom", prenom);
+					record.put("Nom", nom);
+					record.put("Année", anneeNaissance);
+					res.put(record);
+				}
 			} catch(Exception e) {
-				
+				System.out.println(e.getMessage());
 			}
 		}
 		return res;
@@ -51,17 +66,19 @@ public class TestConnection {
 
 	public static void main(String[] args) {
 		System.out.println("Tous les enregistrements :");
-		JSONObject j1=new JSONObject();
+		JSONObject j1 = new JSONObject();
 		j1.put("commande", "*");
 		System.out.println(TestConnection.recupere(j1));
+		System.out.println('\n');
 
-		
 
-		System.out.println("nés avant 1998 :");
-		JSONObject j2=new JSONObject();
+		System.out.println("Toutes les personnes nées avant 1998 :");
+		JSONObject j2 = new JSONObject();	
+
 		j2.put("commande", "<=");
 		j2.put("valeur", 1998);
 		System.out.println(TestConnection.recupere(j2));
+		System.out.println('\n');
 	}
 
 }
